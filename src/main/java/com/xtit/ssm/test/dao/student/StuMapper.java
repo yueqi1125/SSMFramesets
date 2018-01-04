73c17255dceebc4499e22d02fbf92520d9a1542c
yueqi1125 <yueqi1125@qq.com>
2018-01-04 15:13:06 +0800

    创建ioc分支，增加对应的service层注入配置
    Signed-off-by: yueqi1125 <yueqi1125@qq.com>

--- src/main/java/com/xtit/ssm/test/dao/student/StuMapper.java
+++ src/main/java/com/xtit/ssm/test/dao/student/StuMapper.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Repository;
 
 import com.xtit.ssm.test.entity.StuInfo;
 
-@Repository
+@Repository("stuDao")
 public interface StuMapper {
 
 	public StuInfo queryStuInfoByNo(String stu_no);
--- src/main/java/com/xtit/ssm/test/service/student/StuService.java
+++ src/main/java/com/xtit/ssm/test/service/student/StuService.java
@@ -1,18 +1,18 @@
 package com.xtit.ssm.test.service.student;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.xtit.ssm.test.dao.student.StuMapper;
 import com.xtit.ssm.test.entity.StuInfo;
 
-@Service
 public class StuService implements IStuService{
 
-	@Autowired
-	public StuMapper stu;
+	public StuMapper stuDao;
 	
 	public StuInfo getStuInfoByNo(String stu_no){
-		return stu.queryStuInfoByNo(stu_no);
+		return stuDao.queryStuInfoByNo(stu_no);
 	}
+
+	public void setStuDao(StuMapper stuDao) {
+		this.stuDao = stuDao;
+	}
+	
 }
